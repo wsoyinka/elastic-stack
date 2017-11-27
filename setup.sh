74fe7cbfcca1d6113597b5e6d856ac0a14279525
Wale Soyinka <wsoyinka>
2017-11-27 19:31:47 +0000

    increase host vm_max_map_count

--- setup.sh
+++ setup.sh
@@ -8,7 +8,11 @@ apt-get update
 apt-cache policy docker-ce
 apt-get install -y docker-ce
 apt-get install -y docker-compose
+usermod -aG docker ubuntu
+sysctl -w vm.max_map_count=262144
+git clone https://github.com/wsoyinka/elastic-stack.git
+cd elastic-stack/
+docker-compose up elastic-stack
 #systemctl status docker
 #sudo groupadd docker
-usermod -aG docker ubuntu
-docker pull wsoyinka/elastic-stack
+#docker pull wsoyinka/elastic-stack
